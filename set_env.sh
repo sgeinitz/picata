abc3f12d9cb56f9776d120047f58106604ebbe33
nryan100 <nicholasryan100@gmail.com>
2022-02-12 12:53:26 -0700

    Adding handy script

---
+++ set_env.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+# Set these variables to your organizations canvas instance & API token
+export CANVAS_URL=<insert canvas URL here>
+export CANVAS_TOKEN=<insert canvas API token here> 
+
+echo "Set environment variables for this session"
+echo $CANVAS_URL $CANVAS_TOKEN 
