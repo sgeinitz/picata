72ffffac326d22b76b6293a4f1f21f9f1a64c53a
steve geinitz <geinitz@msudenver.edu>
2023-05-30 17:28:51 -0600

    fixed picata_config lint errors

--- picata_config.py
+++ picata_config.py
@@ -1,9 +1,11 @@
 import os
 
+
 class PicataConfig:
     """ Picata configuration class, mostly for file paths """
 
     def __init__(self):
+        """ Simple file path configuration for quiz data to be saved. """
         self.path = os.getcwd()
-        self.prefix =  "quiz_"
+        self.prefix = "quiz_"
         self.file_prefix = self.path + "/" + self.prefix
